3a8cf7b0837737d87c3cf6c82427b36345710a96
Kumarsn680 <kumarsn680@gmail.com>
2023-03-19 15:44:49 +0530

    Checking gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react'
-import './App.css'
+import {  useState } from 'react'
+
 
 function App() {
   const [board,setBoard] = useState(["","","","","","","","",""])
